853a2fd07ac7834ae61275cce7474ee3aea9d1cc
John Raines <jraines@taskrabbit.com>
2024-08-07 10:41:17 -0500

    Added a helper run script

---
+++ examples/project/hello_printer/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# NOTE: this helper script assumes you have open ai credentials stored in ~/.credentials/openai-api-key
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export OPENAI_API_KEY=$(cat ~/.credentials/openai-api-key)
+OPENAI_API_KEY=$OPENAI_API_KEY ragmatic summarize --config $SCRIPT_DIR/example-config.yaml
