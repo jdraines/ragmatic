2853ce8c4a58083b426a597f3c3bdc86526f709e
John Raines <jraines@taskrabbit.com>
2024-08-23 11:03:52 -0500

    add a plugin hook to the hello_printer example

--- examples/hello_printer/src/hello_printer/registry.py
+++ examples/hello_printer/src/hello_printer/registry.py
@@ -1,3 +1,4 @@
+import importlib
 from typing import Type
 from .printer_base import PrinterBase
 from .caps_printer import CapsPrinter
@@ -8,6 +9,12 @@ _printers = {
     CapsPrinter.name: CapsPrinter,
     LowerCasePrinter: LowerCasePrinter
 }
+
+def import_and_register_printer(printer_import_path: str):
+    module_path, printer_name = printer_import_path.rsplit(".", 1)
+    module = importlib.import_module(module_path)
+    printer_cls = getattr(module, printer_name)
+    register_printer(printer_cls)
 
 def register_printer(printer_cls: Type[PrinterBase]):
     _printers[printer_cls.name] = printer_cls
