e06f53f76af4e8bd2aa9dd8d9f986a0852c85464
John Raines <jraines@taskrabbit.com>
2024-08-23 10:59:53 -0500

    updating examples

--- examples/hello_printer/run-rag.sh
+++ examples/hello_printer/run-rag.sh
@@ -2,5 +2,7 @@
 # NOTE: this helper script assumes you have open ai credentials stored in ~/.credentials/openai-api-key
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export OPENAI_API_KEY=$(cat ~/.credentials/openai-api-key)
-ragmatic rag-query --config $SCRIPT_DIR/example-config.yaml \
+export TOKENIZERS_PARALLELISM=false
+ragmatic rag-query \
+    --config $SCRIPT_DIR/example-config.yaml \
     --query "How could I extend behavior of this codebase to perform some special operation following the open/closed principle?"
--- examples/hello_printer/src/hello_printer/printer_base.py
+++ examples/hello_printer/src/hello_printer/printer_base.py
@@ -3,9 +3,6 @@
 class PrinterBase:
 
     name = "base"
-    
-    def __init__(self):
-        self.name = 'PrinterBase'
 
     def print(self, message):
         raise NotImplementedError
